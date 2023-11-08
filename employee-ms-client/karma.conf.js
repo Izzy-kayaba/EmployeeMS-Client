bd84f23f2134f36a94a700550ed7517049feee98
izzy-kayaba <israelkayaba@gmail.com>
2023-11-08 22:24:16 +0200

    completed

--- employee-ms-client/karma.conf.js
+++ employee-ms-client/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/employee-ms-client'),
+      dir: require('path').join(__dirname, './coverage/management-system'),
       subdir: '.',
       reporters: [
         { type: 'html' },
